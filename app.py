b5e28bc43aeb8127b011f98bfd96f5f2676d11c9
Md Nazrul Islam <arfanislamabir@gmail.com>
2024-03-30 00:24:49 +0600

    data ingestion done

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
-from signLanguage.logger import logging
-from signLanguage.exception import SignException
-import sys
+import sys, os
+from signLanguage.pipeline.training_pipeline import TrainPipeline
 
 
-logging.info("Welcome to the CV project")
-
-
-try:
-  a = 7 / '9'
-
-except Exception as e:
-  raise SignException(e, sys) from e
+obj = TrainPipeline()
+obj.run_pipeline()
